838c75e1adf278983c0006eddcae0c267306d140
Akhil <legitthug54@gmail.com>
2020-03-26 22:05:31 -0400

    add swipe left list functionality using Swipeout

--- Lists.js
+++ Lists.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet,  ScrollView, Image, Button } from 'react-native'
+import { Text, TouchableOpacity,FlatList, View, StyleSheet,  ScrollView, Image, Button } from 'react-native'
 // import ImagePicker from 'react-native-image-picker'
 import * as ImagePicker from 'expo-image-picker';
+import uuid from 'react-native-uuid';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import Swipeable from 'react-native-gesture-handler';
+import Swipeout from 'react-native-swipeout'
+// import {GestureHandler} from 'expo';
+// const { Swipeable } = GestureHandler;
+// import {
+//    TouchableNativeFeedback,
+//    TouchableHighlight,
+//    TouchableOpacity,
+//    TouchableWithoutFeedback
+//   } from 'react-native-gesture-handler'
+
 
 class List extends Component {
    state = {
@@ -12,63 +25,109 @@ class List extends Component {
    alertItemName = (item) => {
       alert(item.name)
    }
-   // handleChoosePhoto = () => {
-      // ImagePicker.launchImageLibrary(options,response => {
-      //    if(response.uri){
-      //       this.setState(state => {
-      //          newPicture = {
-      //             path:response.uri,
-      //             name:'whatev',
-      //             id:5
-      //          }
-      //          const names = state.names.concat(state.value);
-      //          return {
-      //            names,
-      //          };
-      //        });
-      //    }
-      // })
-      pickImage = async () => {
-         let result = await ImagePicker.launchImageLibraryAsync({
-           mediaTypes: ImagePicker.MediaTypeOptions.All,
-           allowsEditing: true,
-           aspect: [4, 3],
-           quality: 1
-         });
-     
-         console.log(result);
-         var newPicture = {
-            id:5,
-            name:'akhil',
-            path:result.uri
-         }
+   pickImage = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+         mediaTypes: ImagePicker.MediaTypeOptions.All,
+         allowsEditing: true,
+         aspect: [4, 3],
+         quality: 1
+      });
+   
+      console.log(result);
+      var newPicture = {
+         id:uuid.v1(),
+         name:'akhil',
+         path:result.uri
+      }
+      if(!result.cancelled){
          this.setState({ names: [...this.state.names, newPicture] }) 
-       }
+      }
+   }
+   onSwipeLeft(gestureState) {
+      console.log('you swiped left')
+   }
+   // RightActions = (progress,dragX) => {
+   //    const scale = dragX.interpolate({
+   //       inputRange:[-100,0],
+   //       outputRange:[1,0],
+   //       extrapolate:'clamp'
+   //    });
+   //    return (
+   //       <View style = {styles.LeftAction}>
+   //          <Text style = {styles.deleteText}>Delete</Text>
+   //       </View>
+   //    );
+   // }
 
    // }
+   clickedImage = (props,id) =>{
+      this.props.navigation.navigate('swiping')
+      console.log(id)
+   }
    render() {
+      const swipeProps = {
+         autoClose:true,
+         onClose:(sectionID, rowId, direction)=>{
+            console.log('closed')
+         },
+         onOpen:(sectionID, rowId, direction)=>{
+            console.log(rowId)
+         },
+         // rowId:this.props.index,
+      }
       return (
          <View>
             <Button title = "choose photo" onPress = {this.pickImage} style = {styles.button}/>
-            <ScrollView>   
-               {
-                  this.state.names.map((item, index) => (
+            <ScrollView>
+            <FlatList
+               data={this.state.names}
+               renderItem={({ item }) => {
+               return(
+                  <Swipeout autoClose={true} right={[
+                     {
+                        onPress: () =>{
+                           console.log(item.id)
+                           // const index = this.state.indexOf(item.id);
+                           // console.log(index)
+                           // newNames = this.state.names.splice(index,1)
+                           // console.log(newNames)
+                           // this.setState({names:newNames})
+                           this.setState({names: this.state.names.filter( (picture) => { 
+                              return picture.id !== item.id
+                           })});
+                           console.log(this.state.names)
+                        },
+                        text:'delete',
+                        type:'delete'
+                     },
+                     {
+                        onPress: () =>{
+                           console.log(item.id)
+                        },
+                        text:'edit',
+                        type:'secondary'
+                     },
+                  ]}>
                      <TouchableOpacity
                         key = {item.id}
                         style = {styles.container}
-                        onPress = {() => this.props.navigation.navigate('swiping')}>
+                        onPress = {() => this.clickedImage(this.props,item.id)}>
                         <Text style = {styles.text}>
-                           {item.name}
+                           Lebron
                         </Text>
                         <Image source = {{uri:item.path}} style = {styles.image} />
                      </TouchableOpacity>
-                  ))
-               }
+                  </Swipeout>
+                  )
+               }}
+               keyExtractor={item => item.id}
+            />   
             </ScrollView>
          </View>
       )
    }
 }
+
 export default List
 
 const styles = StyleSheet.create ({
@@ -93,5 +152,9 @@ const styles = StyleSheet.create ({
       top:0,
       right:0,
       position: 'absolute'
+   },
+   LeftAction:{
+      backgroundColor:'red',
+      padding:30
    }
 })
