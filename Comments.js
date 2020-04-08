4040b81330632dd39446c3970714f46c533e1e1e
akhil1213 <legitthug54@gmail.com>
2020-04-08 18:17:35 -0400

    working on cloning instagram comment functionality

--- Comments.js
+++ Comments.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { TextInput,Modal,Dimensions, Text, TouchableOpacity,FlatList, View, StyleSheet,  ScrollView, Image, Button } from 'react-native'
+import { TextInput,Modal,Dimensions, Keyboard, Text, TouchableOpacity,FlatList, View, StyleSheet,  ScrollView, Image, Button, Animated } from 'react-native'
 import {connect, dispatch} from 'react-redux'
 import { useIsFocused } from '@react-navigation/native'
 import uuid from 'react-native-uuid'
@@ -10,30 +10,56 @@ class Comments extends Component {
         super(props)
         this.params = this.props.route.params
     }
-    state={
-        commentText:''
+    componentWillMount() {
+        this.keyboardDidShowSub = Keyboard.addListener('keyboardDidShow', this.handleKeyboardDidShow);
+        this.keyboardDidHideSub = Keyboard.addListener('keyboardDidHide', this.handleKeyboardDidHide);
+      }
+    
+      componentWillUnmount() {
+        this.keyboardDidShowSub.remove();
+        this.keyboardDidHideSub.remove();
+      }
+      state={
+        commentText:'',
+        keyboardHeight:new Animated.Value(20)
+        }
+        //when your translating y and if your going from bottom up it should be a negative value. 
+    handleKeyboardDidShow = (event) =>{
+        console.log("Keyboard called")
+        Animated.timing(this.state.keyboardHeight, {
+            duration: 0,
+            toValue: -1*(event.endCoordinates.height-30),
+            useNativeDriver: true,
+        }).start();
+        // this.setState({keyboardHeight:event.endCoordinates.height})
+        // console.log("just seen thsdfdsfe keyboard dawg")
     }
     buttonPressed = () =>{
-        console.log(this.params)
-        var commentInfo= {
-            commentText:this.state.commentText,
-            pictureId:this.params.picture.item.id,
-            id:uuid.v1(),
-            liked:false
+        if(this.state.commentText!=''){
+            console.log(this.params)
+            var commentInfo= {
+                commentText:this.state.commentText,
+                pictureId:this.params.picture.item.id,
+                id:uuid.v1(),
+                liked:false
+            }
+            this.props.addComment(commentInfo)
+            this.props.navigation.goBack()
         }
-        this.props.addComment(commentInfo)
-        this.props.navigation.goBack()
     }
     render(){
         // const { params } = this.props.navigation.state;
         // const addComment = params ? params.addComment : null;
+        //you need to use animated view since both the text height and the location of the input field should change in parallel. Otherwise location of input field has to wait for text to finish.
         return (
-            <View style={styles.container}>
-                <TextInput placeholder = "Add a comment..." style={styles.input} onChangeText={(text) => {this.setState({commentText:text})}}/>
-                <TouchableOpacity onPress = {this.buttonPressed.bind(this)} style={styles.postButton}>
-                    <Text styles={{color:'rgb(122, 136, 204)'}}>Post</Text>
-                </TouchableOpacity>
-            </View>
+            <Animated.View style={[styles.container, { transform: [{translateY: this.state.keyboardHeight}] }]}>
+                <TextInput multiline={true} placeholder = "Add a comment..." onChangeText={(text) => {this.setState({commentText:text})}}/>
+                <View style={styles.postButton}>
+                    <TouchableOpacity onPress = {this.buttonPressed.bind(this)} style={styles.postButton}>
+                        <Text style={{color:'rgb(122, 136, 204)'}}>Post</Text>
+                    </TouchableOpacity>
+                </View>
+            </Animated.View>
         )
     }
 }
@@ -44,12 +70,12 @@ const styles = StyleSheet.create ({
         borderWidth:1,
         flexDirection:'row',
         position:'absolute',
-        bottom:30,
-        width:'100%'
+        width:'100%',
+        bottom:30
     },
     postButton:{
-        position:'absolute',
-        right:0,
+        paddingLeft:100,
+        color:'rgb(122,136,204)'
     }
 })
 
